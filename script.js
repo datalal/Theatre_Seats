cb0507743c3c2fc5da5d901629352a244901951a
datalal <lawsonw2@gmail.com>
2017-02-14 09:08:40 -0500

    init publish
    init publish

--- script.js
+++ script.js
@@ -19,8 +19,17 @@ container.appendChild(seatingHeader);
 function displaySeats() {
     //var usersName = document.getElementById("userName");
     // console.log(usersName);
+for( i = 1; i < 7; i++){
 
-    for (i = 1; i < 25; i++) {
+// move(append) into a row div with seats
+
+  var rowCount = document.createElement("div");
+  rowCount.className = "rowCount";
+  rowCount.textContent = "Row " + i;
+  rowCount.id = "rowCount";
+  container.appendChild(rowCount);
+
+    for (j = 1; j < 5; j++) {
 
         seatInfo.push({
             userName: "testUser",
@@ -30,35 +39,101 @@ function displaySeats() {
 
         seatBox = document.createElement("div");
         seatBox.className = "seatBox";
-        seatBox.textContent = "seatBox" + seatInfo[i].seatNumber;
+        seatBox.textContent = "Seat " + j;
+        // seatBox.textContent = "seatBox" + " " + j + " " + seatInfo[i].seatNumber;
+
         seatBox.id = "seatBox";
         container.appendChild(seatBox);
         // seatInfo[0].reserved = true;
         // seatInfo[0].seatNumber = i;
 
-    }
+    };
+
+
+
+  };
 };
 
 
 console.log(seatInfo);
 
 displaySeats();
-// reserveFormLoad();
-
-// var activeSeat = document.getElementById("seatBox").innerHTML;
-document.getElementById("seatBox").onclick = reserveFormLoad();
-
-
-console.log("whoa");
 
 var stage = document.createElement("div");
 stage.className = "stage";
 stage.textContent = "stage";
 container.appendChild(stage);
+// reserveFormLoad();
 
-function reserveFormLoad() {
-    var reserveForm = document.createElement("div");
+// var activeSeat = document.body.onclick.textContent;
+ document.getElementById("seatBox").onclick = reserveFormLoad();
+
+
+console.log("whoa");
+reserveSeat();
+
+
+function reserveFormLoad(seat) {
+
+  var formLoad = document.createElement("form");
+  formLoad.className = "formLoad";
+formLoad.textContent = "Reserve A Seat";
+  container.appendChild(formLoad);
+
+
+  var lineBreak = document.createElement("p");
+  lineBreak.className = "lineBreak";
+  // lineBreak.textContent = "lineBreak";
+  formLoad.appendChild(lineBreak);
+
+  var userNameText = document.createElement("input");
+  userNameText.className = "userNameText";
+  userNameText.type = "text";
+  userNameText.id = "userNameText";
+  userNameText.placeholder = "Please Enter Name";
+  formLoad.appendChild(userNameText);
+
+  var lineBreak = document.createElement("p");
+  lineBreak.className = "lineBreak";
+  // lineBreak.textContent = "lineBreak";
+  formLoad.appendChild(lineBreak);
+
+  var reserveCheck = document.createElement("input");
+  reserveCheck.className = "reserveCheck";
+  reserveCheck.textContent = "reserveCheck";
+  reserveCheck.type = "checkbox";
+  reserveCheck.name = "reserveCheck";
+  // reserveCheck.placeholder = "Please Enter Name";
+  formLoad.appendChild(reserveCheck);
+
+    var reserveForm = document.createElement("span");
     reserveForm.className = "reserveForm";
-    reserveForm.textContent = "reserveForm" + '<p>' + "TEST P";
-    container.appendChild(reserveForm);
+    reserveForm.textContent = " Reserve Seat?";
+    formLoad.appendChild(reserveForm);
+
+    var lineBreak = document.createElement("p");
+    lineBreak.className = "lineBreak";
+    // lineBreak.textContent = "lineBreak";
+    formLoad.appendChild(lineBreak);
+
+    var submitButton = document.createElement("button");
+    submitButton.className = "submitButton";
+    submitButton.textContent = "Submit";
+    submitButton.type = "button";
+    // submitButton.onclick = reserveSeat();
+    formLoad.appendChild(submitButton);
+
+
+};
+
+function reserveSeat() {
+
+
+var userNameInput = document.getElementById("userNameText").value;
+// var reserveCheckBox = document.querySelector('input[name="reserveCheck"]:checked').value;
+
+console.log(userNameInput);
+
+console.log("who hey sets");
+
 };
